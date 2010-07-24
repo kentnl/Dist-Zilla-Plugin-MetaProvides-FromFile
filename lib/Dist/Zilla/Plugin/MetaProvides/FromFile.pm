e1a33ca8f35abb5c9bd0aa4742f2c95c96bc8126
Kent Fredric <kentfredric@gmail.com>
2010-07-25 01:22:37 +1200

    Build results of d2087fb (on master)

--- lib/Dist/Zilla/Plugin/MetaProvides/FromFile.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides/FromFile.pm
@@ -2,6 +2,9 @@ use strict;
 use warnings;
 
 package Dist::Zilla::Plugin::MetaProvides::FromFile;
+BEGIN {
+  $Dist::Zilla::Plugin::MetaProvides::FromFile::VERSION = '1.10034201';
+}
 
 # ABSTRACT: In the event nothing else works, pull in hand-crafted metadata from a specified file.
 #
@@ -12,56 +15,19 @@ use Carp                ();
 use Config::INI::Reader ();
 use Dist::Zilla::MetaProvides::ProvideRecord;
 
-=head1 ROLES
-
-=head2 L<Dist::Zilla::Role::MetaProvider::Provider>
-
-=cut
 
 use namespace::autoclean;
 with 'Dist::Zilla::Role::MetaProvider::Provider';
 
-=head1 PLUGIN FIELDS
-
-=head2 file
-
-=head3 type: required, ro, Str
-
-=cut
 
 has file => ( isa => 'Str', is => 'ro', required => 1, );
 
-=head2 reader_name
-
-=head3 type: ClassName, ro.
-
-=head3 default: Config::INI::Reader
-
-=cut
 
 has reader_name => ( isa => 'ClassName', is => 'ro', default => 'Config::INI::Reader', );
 
-=head1 PRIVATE PLUGIN FIELDS
-
-=head2 _reader
-
-=head3 type: Object, ro, built from L</reader_name>
-
-=cut
 
 has _reader => ( isa => 'Object', is => 'ro', lazy_build => 1, );
 
-=head1 ROLE SATISFYING METHODS
-
-=head2 provides
-
-A conformant function to the L<Dist::Zila::Role::MetaProvider::Provider> Role.
-
-=head3 signature: $plugin->provides()
-
-=head3 returns: Array of L<Dist::Zilla::MetaProvides::ProvideRecord>
-
-=cut
 
 sub provides {
   my $self      = shift;
@@ -77,17 +43,64 @@ sub provides {
   return $conf->keys->map($to_record)->flatten;
 }
 
-=head1 BUILDER METHODS
-
-=head2 _build__reader
-
-=cut
 
 sub _build__reader {
   my ($self) = shift;
   eval "require " . $self->reader_name . "; 1;" or die;
   return $self->reader_name->new();
 }
+
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::Plugin::MetaProvides::FromFile - In the event nothing else works, pull in hand-crafted metadata from a specified file.
+
+=head1 VERSION
+
+version 1.10034201
+
+=head1 ROLES
+
+=head2 L<Dist::Zilla::Role::MetaProvider::Provider>
+
+=head1 PLUGIN FIELDS
+
+=head2 file
+
+=head3 type: required, ro, Str
+
+=head2 reader_name
+
+=head3 type: ClassName, ro.
+
+=head3 default: Config::INI::Reader
+
+=head1 PRIVATE PLUGIN FIELDS
+
+=head2 _reader
+
+=head3 type: Object, ro, built from L</reader_name>
+
+=head1 ROLE SATISFYING METHODS
+
+=head2 provides
+
+A conformant function to the L<Dist::Zila::Role::MetaProvider::Provider> Role.
+
+=head3 signature: $plugin->provides()
+
+=head3 returns: Array of L<Dist::Zilla::MetaProvides::ProvideRecord>
+
+=head1 BUILDER METHODS
+
+=head2 _build__reader
 
 =head1 SEE ALSO
 
@@ -97,8 +110,16 @@ sub _build__reader {
 
 =back
 
-=cut
+=head1 AUTHOR
 
-__PACKAGE__->meta->make_immutable;
-1;
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Kent Fredric.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
 
