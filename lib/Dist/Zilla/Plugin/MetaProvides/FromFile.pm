7bcf2d0e81a1730dcd3d56afef8fea903df85244
Kent Fredric <kentfredric@gmail.com>
2014-07-31 16:29:56 +1200

    Documentation::RequirePodLinksIncludeText

--- lib/Dist/Zilla/Plugin/MetaProvides/FromFile.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides/FromFile.pm
@@ -89,7 +89,7 @@ It can be substituted by any class name that matches the following criteria
 
 =head1 ROLES
 
-=head2 L<Dist::Zilla::Role::MetaProvider::Provider>
+=head2 L<< C<::Role::MetaProvider::Provider>|Dist::Zilla::Role::MetaProvider::Provider >>
 
 =cut
 
@@ -130,11 +130,11 @@ has _reader => ( isa => 'Object', is => 'ro', lazy_build => 1, );
 
 =head2 provides
 
-A conformant function to the L<Dist::Zila::Role::MetaProvider::Provider> Role.
+A conformant function to the L<< C<::Role::MetaProvider::Provider>|Dist::Zila::Role::MetaProvider::Provider >> Role.
 
 =head3 signature: $plugin->provides()
 
-=head3 returns: Array of L<Dist::Zilla::MetaProvides::ProvideRecord>
+=head3 returns: Array of L<< C<MetaProvides::ProvideRecord>|Dist::Zilla::MetaProvides::ProvideRecord >>
 
 =cut
 
@@ -168,7 +168,7 @@ sub _build__reader {
 
 =over 4
 
-=item * L<Dist::Zilla::Plugin::MetaProvides>
+=item * L<< C<[MetaProvides]>|Dist::Zilla::Plugin::MetaProvides >>
 
 =back
 
